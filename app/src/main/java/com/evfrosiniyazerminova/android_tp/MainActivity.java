dc6a6a1cf04fb674e65cd09348512ee12d09a0bf
Evfrosiniya <evfrosiniya.z@gmail.ru>
2017-03-08 00:51:37 +0300

    News intent service. Refresh news.

--- app/src/main/java/com/evfrosiniyazerminova/android_tp/MainActivity.java
+++ app/src/main/java/com/evfrosiniyazerminova/android_tp/MainActivity.java
@@ -1,16 +1,57 @@
 package com.evfrosiniyazerminova.android_tp;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import ru.mail.weather.lib.News;
 import ru.mail.weather.lib.NewsLoader;
+import ru.mail.weather.lib.Storage;
 import ru.mail.weather.lib.Topics;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button settingsButton, refreshButton, updateButton;
+    TextView titleTextView, textTextView, dateTextView;
+    Storage mStorage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        settingsButton = (Button) findViewById(R.id.settingsButton);
+        refreshButton = (Button) findViewById(R.id.refreshButton);
+        updateButton = (Button) findViewById(R.id.updateButton);
+
+        titleTextView = (TextView) findViewById(R.id.newsTitle);
+        textTextView = (TextView) findViewById(R.id.newsText);
+        dateTextView = (TextView) findViewById(R.id.newsDate);
+
+        settingsButton.setOnClickListener(onSettingsClick);
+        refreshButton.setOnClickListener(onRefreshClick);
+
+        mStorage = Storage.getInstance(MainActivity.this);
+
+        if (mStorage.loadCurrentTopic() == "" || mStorage.loadCurrentTopic() == null)
+            mStorage.saveCurrentTopic(Topics.AUTO);
+
+
+
+    }
 
     private final View.OnClickListener onSettingsClick = new View.OnClickListener() {
         @Override
@@ -20,22 +61,45 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-
-
+    private final View.OnClickListener onRefreshClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, NewsIntentService.class);
+            intent.setAction(NewsIntentService.NEWS_LOAD_ACTION);
+            startService(intent);
+        }
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onStart() {
+        super.onStart();
+        final IntentFilter filter = new IntentFilter();
+        filter.addAction(NewsIntentService.NEWS_CHANGED_ACTION);
+        filter.addAction(NewsIntentService.NEWS_ERROR_ACTION);
+        LocalBroadcastManager.getInstance(MainActivity.this).registerReceiver(receiver, filter);
 
-        findViewById(R.id.settingsButton).setOnClickListener(onSettingsClick);
 
-        try {
-                NewsLoader newsLoader = new NewsLoader();
-                newsLoader.loadNews(Topics.AUTO);
-            }
-        } catch(IOException) {
-            ie.printStackTrace();
+        News news = mStorage.getLastSavedNews();
+        if (news != null) {
+            titleTextView.setText(news.getTitle());
+            textTextView.setText(news.getBody());
+            dateTextView.setText(DateFormat.format("MM/dd/yyyy HH:mm:ss", new Date(news.getDate())).toString());
         }
     }
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            if (intent.getAction().equals(NewsIntentService.NEWS_CHANGED_ACTION)) {
+                News news = mStorage.getLastSavedNews();
+                titleTextView.setText(news.getTitle());
+                textTextView.setText(news.getBody());
+                dateTextView.setText(DateFormat.format("MM/dd/yyyy HH:mm:ss", new Date(news.getDate())).toString());
+
+            }
+            if (intent.getAction().equals(NewsIntentService.NEWS_ERROR_ACTION)) {
+                textTextView.setText("ERROR");
+            }
+        }
+    };
 }
---
+++ app/src/main/java/com/evfrosiniyazerminova/android_tp/NewsIntentService.java
@@ -0,0 +1,51 @@
+package com.evfrosiniyazerminova.android_tp;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.support.v4.content.LocalBroadcastManager;
+
+import java.io.IOException;
+
+import ru.mail.weather.lib.News;
+import ru.mail.weather.lib.NewsLoader;
+import ru.mail.weather.lib.Storage;
+import ru.mail.weather.lib.Topics;
+
+
+public class NewsIntentService extends IntentService {
+
+    public static final String NEWS_ERROR_ACTION = "NEWS_ERROR_ACTION";
+    public static final String NEWS_LOAD_ACTION = "NEWS_LOAD_ACTION";
+    public static final String NEWS_CHANGED_ACTION = "NEWS_CHANGED_ACTION";
+
+    public NewsIntentService() {
+        super("NewsIntentService");
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(this);
+        if (intent != null & NEWS_LOAD_ACTION.equals(intent.getAction())) {
+
+            try {
+                NewsLoader newsLoader = new NewsLoader();
+
+                News news = newsLoader.loadNews(Storage.getInstance(this).loadCurrentTopic());
+
+                if (news != null) {
+                    Storage.getInstance(this).saveNews(news);
+                    broadcastManager.sendBroadcast(new Intent(NEWS_CHANGED_ACTION));
+                } else {
+                    broadcastManager.sendBroadcast(new Intent(NEWS_ERROR_ACTION));
+                }
+
+            } catch(IOException e) {
+                broadcastManager.sendBroadcast(new Intent(NEWS_ERROR_ACTION));
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
